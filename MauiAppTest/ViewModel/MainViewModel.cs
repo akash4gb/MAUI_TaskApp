3ef881b8af4412563a602cd8f0b27381671f8a58
Akash Khan <akash4gb@gmail.com>
2022-08-28 20:27:29 +0600

    Added nugetPackage community MVVM

---
+++ MauiAppTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+
+namespace MauiAppTest.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    string text;
+
+    public string Text
+    {
+        get => text;
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+}
