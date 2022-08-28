c8d58a6ace6a3d9eb0c245d8bf952f018b4dbcb8
Akash Khan <akash4gb@gmail.com>
2022-08-28 20:55:46 +0600

    binding context with DI

--- MauiAppTest/MainPage.xaml.cs
+++ MauiAppTest/MainPage.xaml.cs
@@ -6,10 +6,10 @@ public partial class MainPage : ContentPage
 {
 	
 
-	public MainPage()
+	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
-		BindingContext = new MainViewModel();
+		BindingContext = vm;
 	}
 
 	
